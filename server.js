3daca34a1f1a19c5f8c0fcbf88f638e27e811149
Oscar Somba <maingioscar2@gmail.com>
2025-04-28 10:56:16 +0300

    finale

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // Points to db.json
 const middlewares = jsonServer.defaults();
+
+// Set default headers to enable CORS
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
